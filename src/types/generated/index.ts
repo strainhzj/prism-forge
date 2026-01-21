918360733e3ac49fa40fbacea74d1a12af469295
strainhzj <hzj774759850@Gmail.com>
2026-01-21 23:06:14 +0800

    feat: 为优化器配置集成 ts-rs 类型生成

---
+++ src/types/generated/index.ts
@@ -0,0 +1,8 @@
+export type { AdvancedConfig } from "./AdvancedConfig";
+export type { CompressionConfig } from "./CompressionConfig";
+export type { FallbackConfig } from "./FallbackConfig";
+export type { LLMParamsConfig } from "./LLMParamsConfig";
+export type { MetaPromptConfig } from "./MetaPromptConfig";
+export type { OptimizerConfig } from "./OptimizerConfig";
+export type { PromptStructureConfig } from "./PromptStructureConfig";
+export type { SessionContextConfig } from "./SessionContextConfig";
